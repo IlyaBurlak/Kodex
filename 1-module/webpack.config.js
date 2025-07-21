b7eba8172862611b4a5423a6f46699fe1546d072
ilaburlak <ilya.burlak.burlak@mail.ru>
2025-07-20 23:14:10 -0700

    refactor: module 4.4
        Настроил Webpack и babel, сборку проекта, dev-сервер, Gzip-сжатие

---
+++ 1-module/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                        },
+                    },
+                ],
+
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html')
+        })
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000,
+        historyApiFallback: true,
+        hot: true,
+        devMiddleware: {
+            publicPath: '/'
+        }
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
