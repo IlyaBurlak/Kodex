b2bde50409e22af8db0c292f1e6aab4cd3e687bd
ilaburlak <ilya.burlak.burlak@mail.ru>
2025-10-07 01:01:07 +0300

    fix: webpack

--- 5-module/src/index.tsx
+++ 5-module/src/index.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-import './index.scss';
 import { StrictMode } from 'react';
 
 const container = document.getElementById('root') as HTMLElement;
